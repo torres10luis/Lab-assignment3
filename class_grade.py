b5454627c45db6dc3bb87ef22222ed1ca44e8abc
Luis Saldana <tluis10a@gmail.com>
2020-12-18 00:07:18 -0800

    first commit

---
+++ class_grade.py
@@ -0,0 +1,26 @@
+
+
+
+grades = []
+
+grade = input("Enter the 1st grade: ")
+grades.append(float(grade))
+
+grade = input("Enter the 2nd grade: ")
+grades.append(float(grade))
+
+grade = input("Enter the 3rd grade: ")
+grades.append(float(grade))
+
+grade = input("Enter the 4th grade: ")
+grades.append(float(grade))
+
+grade = input("Enter the 5th grade: ")
+grades.append(float(grade))
+
+grades.sort()
+grades = grades[2:]
+grades = sum(grades)
+result = grades / 3
+
+print("Average Grade {0:.2f}%".format(result))
